0a7e461bb194382b75e1dd7c45abcb19609dc861
Joel Jensen <jjensen@jgpete.com>
2024-01-20 17:41:15 -0700

    completed Animate cart (backpack) icon when item added to cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,22 @@
 import { setLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 
+function animateCart() {
+  // select the cart backpack icon
+  let bag = document.querySelector(".backpack");
+  // add the animation class to the cart backpack icon
+  bag.classList.add("stuffed");
+
+  // When the animation ends remove the animation class
+  bag.addEventListener(
+    "animationend",
+    () => {
+      bag.classList.remove("stuffed");
+    },
+    { once: true }
+  );
+  // Source: https://www.javascripttutorial.net/dom/events/create-a-one-off-event-handler/
+}
 function addProductToCart(product) {
   setLocalStorage("so-cart", product);
 }
@@ -8,6 +24,7 @@ function addProductToCart(product) {
 async function addToCartHandler(e) {
   const product = await findProductById(e.target.dataset.id);
   addProductToCart(product);
+  animateCart();
 }
 
 // add listener to Add to Cart button
