ac2a558f43351066722540788f4591d81b5411a6
CJLynch01 <cjlynch@byui.edu>
2024-01-29 19:32:39 -0700

    Save Team3

---
+++ src/js/productList.mjs
@@ -0,0 +1,25 @@
+import { getData } from "./productData.mjs"
+
+export default async function productList(category = "tents", selector=".product-list") {
+    const data = await getData(category)
+    renderList(data, selector)
+}
+
+
+function productCardTemplate(product) {
+    return `<li class="product-card">
+    <a href="product_pages/index.html?product=${product.Id}">
+    <img
+      src="${product.Image}"
+      alt="Image of ${product.Name}"
+    />
+    <h3 class="card__brand">${product.Brand.Name}</h3>
+    <h2 class="card__name">${product.Name}</h2>
+    <p class="product-card__price">$${product.ListPrice}</p></a>
+  </li>`
+}
+
+function renderList(productList, selector) {
+    const htmlItems = productList.map((product) => productCardTemplate(product));
+    document.querySelector(selector).innerHTML = htmlItems.join("");
+}
