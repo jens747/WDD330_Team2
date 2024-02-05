da4846414de257066ee52907212b84f3060514d2
CJLynch01 <cjlynch@byui.edu>
2024-02-05 13:21:48 -0700

    Save

--- src/js/product.js
+++ src/js/product.js
@@ -7,19 +7,6 @@ import productDetails from "./productDetails.mjs"
 const productId = getParam("product");
 productDetails(productId);
 
-// const cartCount=document.querySelector('.cart-count');
-//   let itemList = getLocalStorage("so-cart");
-//   console.log(itemList)
-//   let count=itemList.length;
-//   console.log(count)
-//   cartCount.innerHTML=count;
-
-//   if(count==0){
-//     cartCount.style.display='none';
-//   }else{
-//     cartCount.style.display='block';
-//   }
-
 function animateCart() {
   // select the cart backpack icon
   let bag = document.querySelector(".backpack");
