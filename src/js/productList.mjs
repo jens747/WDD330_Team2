6e8bec2d9a473ca40108c86f6ea5d8f39f6b670f
CJLynch01 <cjlynch@byui.edu>
2024-02-04 19:09:09 -0700

    Save

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -7,41 +7,22 @@ export default async function productList(category = "tents", selector=".product
 
 function productCardTemplate(product) {
 
-  // Object.keys(product).forEach(function(key){
-  //   if (product[key] === "989CG") {
-  //     document.getElementById(".product-list").style.visibility = "hidden";
-  //   }
-  // });
-
-    return `<li class="product-card">
-    <a href="product_pages/index.html?product=${product.Id}">
-    <img
-      src="${product.Image}"
-      alt="Image of ${product.Name}"
-    />
-    <h3 class="card__brand">${product.Brand.Name}</h3>
-    <h2 class="card__name">${product.NameWithoutBrand}</h2>
-    <p class="product-card__price">$${product.FinalPrice}</p></a>
-    </li>`;
+    if (product.Id !== "989CG" && product.Id !== "880RT") {
+      return `<li class="product-card">
+      <a href="product_pages/index.html?product=${product.Id}">
+      <img
+        src="${product.Image}"
+        alt="Image of ${product.Name}"
+      />
+      <h3 class="card__brand">${product.Brand.Name}</h3>
+      <h2 class="card__name">${product.NameWithoutBrand}</h2>
+      <p class="product-card__price">$${product.FinalPrice}</p></a>
+      </li>`;
+    }
 }
 
 function renderList(productList, selector) {
-    // Object.keys(product).forEach(function(key){
-    // if (product[key] === "989CG") {
-    //   document.getElementById(".product-list").style.visibility = "hidden";
-    // } else {
-    // const filterItems = productList.filter(product)
-    // productList.filter(products, ["ID", "880RR"])
+
     const htmlItems = productList.map((product) => productCardTemplate(product));
-    // const newhtmlItems = htmlItems.filter(products, [ "Id" , "880RR"])
-    // const filterItems = items.reduce()
     document.querySelector(selector).innerHTML = htmlItems.join("");
-
-
-
 }
-
-
-// const cartNum = document.querySelector(".cart");
-//   let count = itemList.length;
-//   cartNum.innerHTML = count;
