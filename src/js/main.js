d128328a2a556595cdf209dd97c2d5ab567a15f9
Joel <jen21092@byui.edu>
2024-02-06 15:01:10 -0700

    finished add customizable alert to index.html

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,10 @@
 import productList from "./productList.mjs";
+import alertList from "./alert.mjs";
+import alertTime from "./alerttime.mjs";
 
 productList();
+
+// jj--add-customizable-alert-to-index.html trello card
+const currentAlerts = alertTime();
+alertList(currentAlerts.currentDay, currentAlerts.timeOfDay);
+// 
