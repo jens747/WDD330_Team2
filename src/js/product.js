2b09b07222671af59cdd9798e8a74afa722fff7f
Joel <jen21092@byui.edu>
2024-02-06 16:36:16 -0700

    resolved getLocalStorage error

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,7 @@
 import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 import { getParam } from "./utils.mjs";
-import { getLocalStorage } from "./utils.mjs";
-import productDetails from "./productDetails.mjs";
+import productDetails from "./productdetails.mjs";
 
 const productId = getParam("product");
 productDetails(productId);
