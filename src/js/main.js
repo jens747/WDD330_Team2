f00e99c132bb92c7834c3bb759b9502092a5d803
Joel <jen21092@byui.edu>
2024-02-10 16:07:09 -0700

    completed call to action trello

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,11 @@ import productList from "./productList.mjs";
 import alertList from "./alert.mjs";
 import alertTime from "./alerttime.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
+import { newUserCheck } from "./register.mjs";
+
+// jj--call-to-action-register-with-site
+newUserCheck();
+// 
 
 productList();
 
