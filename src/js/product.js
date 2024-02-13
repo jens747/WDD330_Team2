f1cb58c6c28aedfea64901f08ace6c98338a3c07
Joel <jen21092@byui.edu>
2024-02-12 20:35:07 -0700

    testing team5

--- src/js/product.js
+++ src/js/product.js
@@ -6,22 +6,22 @@ import productDetails from "./productdetails.mjs";
 const productId = getParam("product");
 productDetails(productId);
 
-function addProductToCart(product) {
-  // get the contents of the cart
-  let storage = getLocalStorage("so-cart");
-  // if there are no contents in the cart, create an
-  // array with the product as the only item
-  if (storage == null) {
-    setLocalStorage("so-cart", [product]);
-  }
-  // If there are contents already, add the product
-  // to the end of the array and set it as the new
-  // value to the same key
-  else {
-    storage.push(product);
-    setLocalStorage("so-cart", storage);
-  }
-}
+// function addProductToCart(product) {
+//   // get the contents of the cart
+//   let storage = getLocalStorage("so-cart");
+//   // if there are no contents in the cart, create an
+//   // array with the product as the only item
+//   if (storage == null) {
+//     setLocalStorage("so-cart", [product]);
+//   }
+//   // If there are contents already, add the product
+//   // to the end of the array and set it as the new
+//   // value to the same key
+//   else {
+//     storage.push(product);
+//     setLocalStorage("so-cart", storage);
+//   }
+// }
 
 // add to cart button event handler
 async function addToCartHandler(e) {
