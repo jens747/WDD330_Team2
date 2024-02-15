91773e4b40ce7934cb2037e2d6a3487daf66d67e
CJLynch01 <cjlynch@byui.edu>
2024-02-15 16:00:01 -0700

    Save

--- src/js/productdetails.mjs
+++ src/js/productdetails.mjs
@@ -11,7 +11,6 @@ export default async function productDetails(productId) {
   // once the HTML is rendered we can add a listener to Add to Cart button
   document.getElementById("addToCart").addEventListener("click", addToCart);
 }
-
 function addToCart() {
   let cartContents = getLocalStorage("so-cart");
   //check to see if there was anything there
@@ -20,24 +19,11 @@ function addToCart() {
   }
   // animate cart
   animateCart();
-  // then if the product is not yet in the cart, add the current product to the list
-  if (cartContents.length == 0) {
-    product.Quantity = 1;
-    cartContents.push(product);
-  }
-  // If it is already in the cart, look for it and increase the quantity by one
-  else {
-    for (let i = 0; i < cartContents.length; i++) {
-      if (cartContents[i].Id == product.Id) {
-        cartContents[i].Quantity += 1;
-      }
-    }
-  }
-  setLocalStorage("so-cart", cartContents);
+  // then add the current product to the list
+      cartContents.push(product);
+    setLocalStorage("so-cart", cartContents);
 }
-  
-
-function renderProductDetails() {
+  function renderProductDetails() {
   document.querySelector("#productName").innerText = product.Brand.Name;
   document.querySelector("#productNameWithoutBrand").innerText =
     product.NameWithoutBrand;
@@ -79,3 +65,33 @@ function animateCart() {
     cartCount.style.display = "block";
   }
 }
+
+// import { findProductById } from "./productData.mjs";
+// import { setLocalStorage } from "./utils.mjs";
+
+// let product = {};
+
+// export default async function productDetails(productId) {
+//   // get the details for the current product. findProductById will return a promise! use await or .then() to process it
+//   product = await findProductById(productId);
+//   // once we have the product details we can render out the HTML
+//   renderProductDetails();
+//   // once the HTML is rendered we can add a listener to Add to Cart button
+//   document.getElementById("addToCart").addEventListener("click", addToCart);
+// }
+// function addToCart() {
+//   setLocalStorage("so-cart", product);
+// }
+// function renderProductDetails() {
+//   document.querySelector("#productName").innerText = product.Brand.Name;
+//   document.querySelector("#productNameWithoutBrand").innerText =
+//     product.NameWithoutBrand;
+//   document.querySelector("#productImage").src = product.Image;
+//   document.querySelector("#productImage").alt = product.Name;
+//   document.querySelector("#productFinalPrice").innerText = product.FinalPrice;
+//   document.querySelector("#productColorName").innerText =
+//     product.Colors[0].ColorName;
+//   document.querySelector("#productDescriptionHtmlSimple").innerHTML =
+//     product.DescriptionHtmlSimple;
+//   document.querySelector("#addToCart").dataset.id = product.Id;
+// }
