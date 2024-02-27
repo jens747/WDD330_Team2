4d2eb845cdd3a32e98ec03427a1cfb6e9d8fa4d6
CJLynch01 <cjlynch@byui.edu>
2024-02-26 19:06:12 -0700

    Save

---
+++ src/js/search.js
@@ -0,0 +1,7 @@
+import productList from "./productList.mjs";
+// import { getParam } from "./utils.mjs";
+
+// const searchcategory = getParam("category");
+// productList(searchcategory, ".product-list");
+
+console.log(productList);
